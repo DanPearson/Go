b3d5b667d993a5e701f0a36a6cf55092e6887004
DanPearson <danny@dannypearson.com>
2023-03-27 18:45:26 +0100

    Update vaultget.go

--- vaultget.go
+++ vaultget.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
+	"fmt"
+	"log"
+	"os"
 
-    "github.com/hashicorp/vault/api"
+	"github.com/hashicorp/vault/api"
+	"github.com/hashicorp/vault/sdk/helper/jsonutil"
 )
 
 func main() {
@@ -19,38 +20,40 @@ func main() {
         log.Fatal("Vault token not set in VAULT_TOKEN environment variable")
     }
 
-    // Create a new Vault client
-    client, err := api.NewClient(&api.Config{
-        Address: vaultAddr,
-    })
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Create a new Vault client
+	client, err := api.NewClient(&api.Config{
+		Address: vaultAddr,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Set the Vault token
-    client.SetToken(vaultToken)
+	// Set the Vault token
+	client.SetToken(vaultToken)
 
-    // Retrieve the secret from Vault
-    secret, err := client.Logical().Read("secret/data/myapp")
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Retrieve the secret from Vault
+	secret, err := client.Logical().Read("secret/data/myapp")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Parse the secret data into a struct
-    var data struct {
-        Username string `json:"username"`
-        Password string `json:"password"`
-    }
-    if secret != nil {
-        if secret.Data != nil {
-            err = secret.Data.DecodeJSON(&data)
-            if err != nil {
-                log.Fatal(err)
-            }
-        }
-    }
+	// Parse the secret data into a struct
+	var data struct {
+		Data struct {
+			Username string `json:"username"`
+			Password string `json:"password"`
+		} `json:"data"`
+	}
+	if secret != nil {
+		if secret.Data != nil {
+			err = jsonutil.DecodeJSON(secret.Data["data"], &data)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
 
-    // Print the secret values
-    fmt.Printf("Username: %s\n", data.Username)
-    fmt.Printf("Password: %s\n", data.Password)
+	// Print the secret values
+	fmt.Printf("Username: %s\n", data.Data.Username)
+	fmt.Printf("Password: %s\n", data.Data.Password)
 }
